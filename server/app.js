eb43c88099d5fc0745441262764f82d552762b62
gagarinzefanya <gagarinzefanya@gmail.com>
2022-09-09 12:21:26 +0700

    init project

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
