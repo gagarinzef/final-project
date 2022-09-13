21c14fc6060111df60eb9eaf6d4769c92e450a01
Ghozi Fattah Allauddin <ghozifattah@gmail.com>
2022-09-13 21:02:31 +0700

    feat: fix redirect after register client

--- client-calendar/src/components/InvitationPage.js
+++ client-calendar/src/components/InvitationPage.js
@@ -40,13 +40,13 @@ function InvitationPage() {
       <a className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-1000">
       <img src="https://i.postimg.cc/s2rkZHpL/icon.jpg" className="w-40 h-40 m-auto mb-10"></img>
       {error ? ( <>
-      <p className="font-normal text-gray-700"> {message} </p>
+      <p className="text-gray-700 font-bold"> {message} </p>
       <img src={loading} className="w-10 h-10 mt-10 mb-10 m-auto"></img>
-        <p className="font-normal text-gray-700 text-xs">
+        <p className="text-gray-700 font-bold text-xs">
          Redirecting...
         </p>
       </>) : ( <>
-    <p className="font-normal text-gray-700">
+    <p className="text-gray-700 font-bold">
          {message}
         </p>
         <img src={loading} className="w-10 h-10 mt-10 mb-10 m-auto"></img>
