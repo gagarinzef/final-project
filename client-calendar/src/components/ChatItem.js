996a512328c81ba2ac4c69dbe5d8fb4072761f6c
gagarinzefanya <gagarinzefanya@gmail.com>
2022-09-14 19:19:03 +0700

    fix conflict

--- client-calendar/src/components/ChatItem.js
+++ client-calendar/src/components/ChatItem.js
@@ -1,92 +1,76 @@
 import React from "react";
 
-const ChatItem = props => {
-    const {
-        user,
-        message,
-        createdAt
-    } = props
+const ChatItem = (props) => {
+  const { user, message, createdAt } = props;
 
-    const style = {
-        display: 'flex',
-        alignItem: 'flex-start',
-        padding: '12px 10px',
-        backgroundColor: 'white',
-        fontSize: '12px',
-        color: 'black',
-        flexDirection: 'column',
-        margin: '5px',
-        borderRadius: 8,
-        overflowWrap: 'anywhere'
-<<<<<<< HEAD
-        
-=======
+  const style = {
+    display: "flex",
+    alignItem: "flex-start",
+    padding: "12px 10px",
+    backgroundColor: "white",
+    fontSize: "12px",
+    color: "black",
+    flexDirection: "column",
+    margin: "5px",
+    borderRadius: 8,
+    overflowWrap: "anywhere",
+  };
 
->>>>>>> fdf42177a7f5ea6fc76e007b617e9268a4cff79b
-    }
+  const formatDate = (date) => {
+    const d = new Date(date);
+    const day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const month = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    return `${day[d.getDay()]}, ${d.getDate()} ${
+      month[d.getMonth()]
+    } ${d.getFullYear()}`;
+  };
 
-    const formatDate = (date) => {
-        const d = new Date(date);
-        const day = [
-            "Sun",
-            "Mon",
-            "Tue",
-            "Wed",
-            "Thu",
-            "Fri",
-            "Sat",
-        ];
-        const month = [
-            "Jan",
-            "Feb",
-            "Mar",
-            "Apr",
-            "May",
-            "Jun",
-            "Jul",
-            "Aug",
-            "Sep",
-            "Oct",
-            "Nov",
-            "Dec",
-        ];
-        return `${day[d.getDay()]}, ${d.getDate()} ${month[d.getMonth()]
-            } ${d.getFullYear()}`;
-    }
-
-
-
-    return <div style={style}>
-        <div>
-            <span style={{
-                float: 'left'
-            }}
-<<<<<<< HEAD
-            ><b>{user}</b></span>
-        </div>
-        <div>
-            <span style={{
-                 float: 'left',
-                 marginTop: 15,
-                 textAlign: 'justify'
-=======
-            ><b>{user}</b> :</span>
-        </div>
-        <div>
-            <span style={{
-                float: 'left'
->>>>>>> fdf42177a7f5ea6fc76e007b617e9268a4cff79b
-            }}
-            >{message}</span>
-        </div>
-        <span style={{
-            fontSize: '9px',
-            textAlign: 'end',
-            marginTop: 20
-        }}>
-            {formatDate(createdAt)}
+  return (
+    <div style={style}>
+      <div>
+        <span
+          style={{
+            float: "left",
+          }}
+        >
+          <b>{user}</b>
         </span>
+      </div>
+      <div>
+        <span
+          style={{
+            float: "left",
+            marginTop: 15,
+            textAlign: "justify",
+          }}
+        >
+          {message}
+        </span>
+      </div>
+      <span
+        style={{
+          fontSize: "9px",
+          textAlign: "end",
+          marginTop: 20,
+        }}
+      >
+        {formatDate(createdAt)}
+      </span>
     </div>
-}
+  );
+};
 
-export default ChatItem
+export default ChatItem;
--- client-calendar/src/components/ChatRoom.js
+++ client-calendar/src/components/ChatRoom.js
@@ -1,156 +1,168 @@
-import axios from 'axios'
-import React, { useEffect, useState, useRef } from 'react'
-import { useParams } from 'react-router-dom'
-import io from 'socket.io-client'
-import ChatItem from './ChatItem'
+import axios from "axios";
+import React, { useEffect, useState, useRef } from "react";
+import { useParams } from "react-router-dom";
+import io from "socket.io-client";
+import ChatItem from "./ChatItem";
 
-const socket = io.connect("http://localhost:3003")
+const socket = io.connect("http://localhost:3003");
 
-const ChatRoom = props => {
+const ChatRoom = (props) => {
+  const [currentProjectId, setCurrentProjectId] = useState(null);
 
-    const [currentProjectId, setCurrentProjectId] = useState(null)
+  const [message, setMessage] = useState("");
 
-    const [message, setMessage] = useState('')
+  const [messageList, setMessageList] = useState([]);
 
-    const [messageList, setMessageList] = useState([])
+  const { projectId } = useParams();
+  const userId = localStorage.getItem("userId");
+  const username = localStorage.getItem("username");
 
-    const { projectId } = useParams()
-    const userId = localStorage.getItem("userId")
-    const username = localStorage.getItem("username")
+  const chatRoomRef = useRef();
 
-    const chatRoomRef = useRef()
+  useEffect(() => {
+    if (currentProjectId === null || currentProjectId !== projectId) {
+      socket.emit("leave_project_chat", currentProjectId);
 
-    useEffect(() => {
-        if (currentProjectId === null || currentProjectId !== projectId) {
-            socket.emit('leave_project_chat', currentProjectId)
+      socket.emit("join_project_chat", projectId);
 
-            socket.emit('join_project_chat', projectId)
+      socket.off("receive_message");
 
-            socket.off('receive_message')
-
-            socket.on('receive_message', data => {
-                setMessageList(prev => [...prev, {
-                    data: {
-                        username: data.data.username,
-                        chat: data.data.chat,
-                        createdAt: data.data.createdAt
-                    }
-                }])
-            })
-            setCurrentProjectId(projectId) //terima watch
-        }
-    }, [projectId])
-
-    useEffect(() => {
-        if (projectId) {
-            axios(
-                `http://localhost:3001/projects/${projectId}/chat`,
-                {
-                    method: "get",
-                    headers: {
-                        access_token: localStorage.getItem("access_token"),
-                    },
-                }
-            ).then(response => {
-                const newMessageList = []
-                response.data.chat.forEach(chat => {
-                    const newChat = {
-                        data: {
-                            username: chat.User.username,
-                            chat: chat.chat,
-                            createdAt: chat.createdAt
-                        }
-                    }
-                    newMessageList.push(newChat)
-                })
-                setMessageList(newMessageList)
-            }).catch(err => console.log(err));
-        }
-
-    }, [projectId])
-
-    useEffect(() => {
-        chatRoomRef.current?.scrollTo(0, chatRoomRef.current?.scrollHeight)
-    }, [messageList]) //buat ngerender ke paling bawah kalo ada perubahan di message list
-
-    const sendMessage = () => {
-        const data = {
-            'user_id': userId,
-            'project_id': projectId,
-            username: username,
-            chat: message,
-            createdAt: new Date()
-        }
-        socket.emit("send_message", { data })
-
-        setMessage('')
+      socket.on("receive_message", (data) => {
+        setMessageList((prev) => [
+          ...prev,
+          {
+            data: {
+              username: data.data.username,
+              chat: data.data.chat,
+              createdAt: data.data.createdAt,
+            },
+          },
+        ]);
+      });
+      setCurrentProjectId(projectId); //terima watch
     }
+  }, [projectId]);
 
-    return <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-        width: '100%',
-<<<<<<< HEAD
-        height: '68vh',
-=======
-        height: '48vh',
->>>>>>> fdf42177a7f5ea6fc76e007b617e9268a4cff79b
-        backgroundColor: 'lightgray',
+  useEffect(() => {
+    if (projectId) {
+      axios(`http://localhost:3001/projects/${projectId}/chat`, {
+        method: "get",
+        headers: {
+          access_token: localStorage.getItem("access_token"),
+        },
+      })
+        .then((response) => {
+          const newMessageList = [];
+          response.data.chat.forEach((chat) => {
+            const newChat = {
+              data: {
+                username: chat.User.username,
+                chat: chat.chat,
+                createdAt: chat.createdAt,
+              },
+            };
+            newMessageList.push(newChat);
+          });
+          setMessageList(newMessageList);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [projectId]);
+
+  useEffect(() => {
+    chatRoomRef.current?.scrollTo(0, chatRoomRef.current?.scrollHeight);
+  }, [messageList]); //buat ngerender ke paling bawah kalo ada perubahan di message list
+
+  const sendMessage = () => {
+    const data = {
+      user_id: userId,
+      project_id: projectId,
+      username: username,
+      chat: message,
+      createdAt: new Date(),
+    };
+    socket.emit("send_message", { data });
+
+    setMessage("");
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-end",
+        width: "100%",
+        height: "56vh",
+        backgroundColor: "lightgray",
         borderRadius: 8,
-    }}>
-        <div id="chat-room" style={{
-            flexOverflow: 'wrap',
-            overflow: 'auto',
-            position: 'relative',
-            width: '100%',
-            height: '45',
-        }} ref={chatRoomRef}>
-            {/* <h1>ProjectId: {projectId}</h1> */}
+      }}
+    >
+      <div
+        id="chat-room"
+        style={{
+          flexOverflow: "wrap",
+          overflow: "auto",
+          position: "relative",
+          width: "100%",
+          height: "45",
+        }}
+        ref={chatRoomRef}
+      >
+        {/* <h1>ProjectId: {projectId}</h1> */}
 
-            {messageList.map((message, index) => {
-                return <ChatItem
-                    key={`chat-item-${index}`}
-                    user={message.data.username}
-                    message={message.data.chat}
-                    createdAt={message.data.createdAt}
-                />
-            })}
-
-        </div>
-        <div style={{
-            display: 'flex',
-            gap: '8px',
-            padding: '10px'
-        }}>
-            <input style={{
-                width: '95%',
-                height: '20px',
-                padding: '5px',
-                fontSize: '12px',
-                color: 'black',
-                borderRadius: 8,
-            }} placeholder='message...' value={message} onChange={event => setMessage(event.target.value)}
-                onKeyPress={event => {
-                    if (event.key === 'Enter') {
-                        sendMessage()
-                    }
-                }}
+        {messageList.map((message, index) => {
+          return (
+            <ChatItem
+              key={`chat-item-${index}`}
+              user={message.data.username}
+              message={message.data.chat}
+              createdAt={message.data.createdAt}
             />
-            <button style={{
-                width: '30px',
-                backgroundColor: 'black',
-                cursor: 'pointer',
-                borderRadius: '5px',
-                fontSize: '12px',
-            }} type='button'
-                onClick={sendMessage}
-            >
-                <i className="far fa-paper-plane"></i>
-            </button>
-        </div>
+          );
+        })}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          gap: "8px",
+          padding: "10px",
+        }}
+      >
+        <input
+          style={{
+            width: "95%",
+            height: "20px",
+            padding: "5px",
+            fontSize: "12px",
+            color: "black",
+            borderRadius: 8,
+          }}
+          placeholder="message..."
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyPress={(event) => {
+            if (event.key === "Enter") {
+              sendMessage();
+            }
+          }}
+        />
+        <button
+          style={{
+            width: "30px",
+            backgroundColor: "black",
+            cursor: "pointer",
+            borderRadius: "5px",
+            fontSize: "12px",
+          }}
+          type="button"
+          onClick={sendMessage}
+        >
+          <i className="far fa-paper-plane"></i>
+        </button>
+      </div>
     </div>
+  );
+};
 
-}
-
-export default ChatRoom
+export default ChatRoom;
