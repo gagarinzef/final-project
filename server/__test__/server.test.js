5a313b5e51d530de0223855ea774283e229a671b
HafizhIslami <hafizhislami11@gmail.com>
2022-09-14 23:09:10 +0700

    update tester

--- server/__test__/server.test.js
+++ server/__test__/server.test.js
@@ -6,7 +6,7 @@ const { queryInterface } = sequelize;
 let access_token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjYyNzk0NDg5fQ.k_mdh2gX9DT8ycWPtIujHmNMjbi4ETwiS5aA_QY4O9c";
 
-jest.setTimeout(500);
+jest.setTimeout(10000);
 
 beforeAll(async () => {
   try {
